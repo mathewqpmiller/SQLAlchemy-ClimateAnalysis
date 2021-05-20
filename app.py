22d1f0f900c54e10a440e7c5ecc29243f4088f01
mathewqpmiller <mathewpqmiller@gmail.com>
2021-05-20 11:52:09 -0700

    Fixed and Completed app.py file

--- app.py
+++ app.py
@@ -24,42 +24,41 @@ from sqlalchemy import create_engine, inspect, func
 # Load flask function that serializes data to Java Open Script Notation format 
 from flask import Flask, jsonify
 
-#################################################
+
 # Database Setup
-#################################################
+##################
 
 # Define file path and create engine
 sqlite_file_path = "Resources/hawaii.sqlite"
-sqlite_engine = create_engine(f"sqlite:///{sqlite_file_path}")
-
+sqlite_engine = create_engine(f"sqlite:///{sqlite_file_path}", connect_args={'check_same_thread': False})
 # Reflect tables, detect and define classes
 base = automap_base()
 base.prepare(sqlite_engine, reflect=True)
 Measurement = base.classes.measurement
 Station = base.classes.station
 
-#################################################
+
 # Flask Setup
-#################################################
+###############
 
 # Start session and initiate Flask
 session = Session(sqlite_engine)
 app = Flask(__name__)
 
-#################################################
+
 # Flask Routes
-#################################################
+################
 
 # Identify All Possible Flask App Routes
 @app.route("/")
 def main():
     # Intro
-    intro =    (f"Available Routes:<br/>"
+    intro =    (f"Welcome to Mathew Miller's Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>")
+        f"/api/v1.0/temp/start/end")
     return (intro)
     
 
@@ -113,84 +112,114 @@ def stations():
     # return jsonify(stations_list)
 
 @app.route("/api/v1.0/tobs")
-def tobs():
+def temp_monthl():
  
     print("Received tobs api request.")
 
-    # Find the most recent date in the data set.
-    latest_dataset_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    latest_dataset_date_print = dt.date(2017,8,23)
+    # # Find the most recent date in the data set.
+    # latest_dataset_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    # latest_dataset_date_print = dt.date(2017,8,23)
     
-    # Define variable that finds the date one year ago to the day from the most recent date in the dataset
-    last_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    # # Define variable that finds the date one year ago to the day from the most recent date in the dataset
+    # last_year = dt.date(2017,8,23) - dt.timedelta(days=365)
 
-    # With the most live station id, query the last 12 months of temperature observations (tobs) 
-    temperature_observations = session.query(Measurement.tobs).\
-                filter(Measurement.date >= last_year).\
-                filter(Measurement.station == "USC00519281").\
-                order_by(Measurement.date).all()
+    # # With the most live station id, query the last 12 months of temperature observations (tobs) 
+    # raw_stations = session.query(Measurement.tobs).\
+    #             filter(Measurement.date >= last_year).\
+    #             filter(Measurement.station == "USC00519281").\
+    #             order_by(Measurement.date).all()
 
-    # Create the tobs list of dictionaries
-    tobs_list = []
-    for result in temperature_observations:
-        tobs_dict = {}
-        tobs_dict["date"] = result.date
-        tobs_dict["station"] = result.station
-        tobs_dict["tobs"] = result.tobs
-        tobs_list.append(tobs_dict)
+    # # Create the tobs list of dictionaries
+    # tobs_list = []
+    # for result in raw_stations:
+    #     tobs_dict = {}
+    #     tobs_dict["date"] = result.date
+    #     tobs_dict["station"] = result.station
+    #     tobs_dict["tobs"] = result.tobs
+    #     tobs_list.append(tobs_dict)
 
-    return jsonify(tobs_list)
+    # return jsonify(tobs_list)
 
-@app.route("/api/v1.0/<start>")
-def start(start):
+     # Calculate the date 1 year ago from last date in database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query the primary station for all tobs from the last year
+    raw_stations = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(raw_stations))
+
+    # Return the results
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/temp/<start>")
+# def start(start):
     
-    print("Received start date api request.")
+#     print("Received start date api request.")
 
-    # Find the most recent date in the data set.
-    latest_dataset_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    latest_dataset_date_print = dt.date(2017,8,23)
+#     # Find the most recent date in the data set.
+#     latest_dataset_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+#     latest_dataset_date_print = dt.date(2017,8,23)
 
-    # Define variable that calculates the minimum, maximum and average temperature from the Measurements class.
-    temp_stats = [func.min(Measurement.tobs),
-              func.max(Measurement.tobs),
-              func.avg(Measurement.tobs)]
+#     # Define variable that calculates the minimum, maximum and average temperature from the Measurements class.
+#     temp_stats = [func.min(Measurement.tobs),
+#               func.max(Measurement.tobs),
+#               func.avg(Measurement.tobs)]
 
-    # Create temperature stats list
-    return_list = []
-    date_dict = {'start_date': start, 'end_date': latest_dataset_date}
-    return_list.append(date_dict)
-    return_list.append({'Observation': 'TMIN', 'Temperature': temp_stats[0][0]})
-    return_list.append({'Observation': 'TAVG', 'Temperature': temp_stats[0][1]})
-    return_list.append({'Observation': 'TMAX', 'Temperature': temp_stats[0][2]})
+#     # Create temperature stats list
+#     return_list = []
+#     date_dict = {'start_date': start, 'end_date': latest_dataset_date}
+#     return_list.append(date_dict)
+#     return_list.append({'Observation': 'TMIN', 'Temperature': temp_stats[0][0]})
+#     return_list.append({'Observation': 'TAVG', 'Temperature': temp_stats[0][1]})
+#     return_list.append({'Observation': 'TMAX', 'Temperature': temp_stats[0][2]})
 
-    return jsonify(return_list)
+#     return jsonify(return_list)
 
-@app.route("/api/v1.0/<start>/<end>")
-def start_end(start, end):
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
   
     print("Received start date and end date api request.")
 
-    # Find the most recent date in the data set.
-    latest_dataset_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    latest_dataset_date_print = dt.date(2017,8,23)
-    
-    # Define variable that finds the date one year ago to the day from the most recent date in the dataset
-    last_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+        # # Find the most recent date in the data set.
+        # latest_dataset_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+        # latest_dataset_date_print = dt.date(2017,8,23)
+        
+        # # Define variable that finds the date one year ago to the day from the most recent date in the dataset
+        # last_year = dt.date(2017,8,23) - dt.timedelta(days=365)
 
-    # Define variable that calculates the minimum, maximum and average temperature from the Measurements class.
+        # Define variable that calculates the minimum, maximum and average temperature from the Measurements class.
     temp_stats = [func.min(Measurement.tobs),
               func.max(Measurement.tobs),
               func.avg(Measurement.tobs)]
     
-    #create a list
-    return_list = []
-    date_dict = {'start_date': last_year, 'end_date': latest_dataset_date}
-    return_list.append(date_dict)
-    return_list.append({'Observation': 'TMIN', 'Temperature': temp_stats[0][0]})
-    return_list.append({'Observation': 'TAVG', 'Temperature': temp_stats[0][1]})
-    return_list.append({'Observation': 'TMAX', 'Temperature': temp_stats[0][2]})
+        # #create a list
+        # return_list = []
+        # date_dict = {'start_date': last_year, 'end_date': latest_dataset_date}
+        # return_list.append(date_dict)
+        # return_list.append({'Observation': 'TMIN', 'Temperature': temp_stats[0][0]})
+        # return_list.append({'Observation': 'TAVG', 'Temperature': temp_stats[0][1]})
+        # return_list.append({'Observation': 'TMAX', 'Temperature': temp_stats[0][2]})
 
-    return jsonify(return_list)
+        # return jsonify(return_list)
+
+    if not end:
+        # calculate TMIN, TAVG, TMAX for dates greater than start
+        raw_stations = session.query(*temp_stats).\
+            filter(Measurement.date >= start).all()
+        # Unravel results into a 1D array and convert to a list
+        temps = list(np.ravel(raw_stations))
+        return jsonify(temps)
+
+    # calculate TMIN, TAVG, TMAX with start and stop
+    raw_stations = session.query(*temp_stats).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(raw_stations))
+    return jsonify(temps=temps)
 
 #code to actually run
 if __name__ == "__main__":
